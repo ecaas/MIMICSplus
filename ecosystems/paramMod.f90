dac23af2f1d85fbed2b95f5a3dbcc4771d09183d
ecaas <ecaas@geo.uio.no>
2020-07-21 12:14:34 +0200

    Clean up + some more variables defined

--- ecosystems/paramMod.f90
+++ ecosystems/paramMod.f90
@@ -3,21 +3,20 @@ use shr_kind_mod   , only : r8 => shr_kind_r8
 !use SoilDecompType, only : carbon_pool_type
 
 implicit none
+
 !Define variables
-
-
-real(kind=r8)                                :: tsoi                            ![degC]
+real(kind=r8),parameter                      :: tsoi =5.0                       ![degC]
 real(kind=r8)                                :: GEP                             ![gC/(m2 h)] Gross ecosystem productivity
 real(kind=r8),parameter                      :: fCLAY  = 0.15                   ![-] fraction of clay in soil
 
 real(r8)                                     :: f_som1=0.05, f_som2=0.05
-real(kind=r8),dimension(3)                   :: k_mycsap                         ![1/h](EcM, ErM, AM) decay constants, MYC to SAP pool
-real(kind=r8),dimension(3)                   :: k_mycsom                         ! [1/h] decay constants, MYC to SOM pools
+real(kind=r8),dimension(3)                   :: k_mycsap                        ! [1/h](EcM, ErM, AM) decay constants, MYC to SAP pool
+real(kind=r8),dimension(3)                   :: k_mycsom                        ! [1/h] decay constants, MYC to SOM pools
 real(kind=r8), parameter                     :: MYC_SAPb=0.4, MYC_SAPf=1-MYC_SAPb![-]Fraction of the flux from mycorrhizal pools to SAPf pool. The rest is going to SAPb
 
 !For calculating the Km parameter in Michaelis Menten kinetics (expressions based on mimics model: https://doi.org/10.5194/gmd-8-1789-2015 and https://github.com/wwieder/MIMICS)
 integer, parameter                           :: MM_eqs  = 6                     !Number of Michaelis-Menten parameters
-real(kind=r8),dimension(MM_eqs),parameter    :: Kslope  = (/0.034, 0.034, 0.034, 0.034, 0.034, 0.034/)!LITm, LITs, SOMa entering SAPb, LITm, LITs, SOMa entering SAPf
+real(kind=r8),dimension(MM_eqs),parameter    :: Kslope  = (/0.034, 0.034, 0.034, 0.034, 0.034, 0.034/) !LITm, LITs, SOMa entering SAPb, LITm, LITs, SOMa entering SAPf
 real(kind=r8),dimension(MM_eqs),parameter    :: Vslope  = (/0.063, 0.063, 0.063, 0.063, 0.063, 0.063/) !LITm, LITs, SOMa entering SAPb, LITm, LITs, SOMa entering SAPf
 real(kind=r8),dimension(MM_eqs),parameter    :: Kint    = (/3.19, 3.19, 3.19, 3.19, 3.19, 3.19/)       !LITm, LITs, SOMa entering SAPb, LITm, LITs, SOMa entering SAPf
 real(kind=r8),dimension(MM_eqs),parameter    :: Vint    = (/5.47, 5.47, 5.47, 5.47, 5.47, 5.47/)       !LITm, LITs, SOMa entering SAPb, LITm, LITs, SOMa entering SAPf
@@ -44,13 +43,15 @@ integer, parameter                           :: pool_types = no_of_litter_pools 
                                                 no_of_sap_pools + no_of_som_pools
 
 !For calculating turnover from SAP to SOM (expressions from mimics model: https://doi.org/10.5194/gmd-8-1789-2015 and  https://github.com/wwieder/MIMICS)
-real(r8)                                :: fMET                            ![-]
 real(r8), dimension(no_of_sap_pools)    :: fPHYS,fCHEM,fAVAIL              ![-]
 real(r8), dimension(no_of_sap_pools)    :: tau                             ![1/h]
-real(r8), dimension(no_of_som_pools), parameter    :: fEcMSOM = (/0.4,0.4,0.2/) !somp,soma,somc. Fraction of flux from EcM to different SOM pools
+
+real(r8)                                :: fMET =0.5                        ![-] Fraction determining distribution of total litter production between LITm and LITs NOTE: Needs revision
+
+real(r8), dimension(no_of_som_pools), parameter    :: fEcMSOM = (/0.4,0.4,0.2/) !somp,soma,somc. Fraction of flux from EcM to different SOM pools NOTE: assumed
 real(r8), dimension(no_of_som_pools), parameter    :: fErMSOM = (/0.2,0.4,0.3/)
 real(r8), dimension(no_of_som_pools), parameter    :: fAMSOM = (/0.3,0.3,0.4/)
-real(r8)                                :: desorb = 3e-4 * exp(-4*(sqrt(fclay)))!1.5e-5 * exp(-1.5*(fclay)) ![1/h]From Mimics, used for the transport from physically protected SOM to available SOM pool
+real(r8)                                :: desorb = 3e-4*exp(-4*(sqrt(fclay)))!1.5e-5 * exp(-1.5*(fclay)) ![1/h]From Mimics, used for the transport from physically protected SOM to available SOM pool
 
 !Depth & vertical transport
 real(r8),parameter                           :: depth = 0.56                    ![m] used if isVertical is False
@@ -61,7 +62,7 @@ real(r8),parameter                           :: D = 1.14e-8![m2/h] Diffusivity. 
 real(r8)                                     :: diffusive_source,diffusive_sink !For use in vertical_diffusion subroutine. Currently using alt_vertical diffusion subroutine
 real(r8),dimension(nlevdecomp,pool_types)    :: net_diffusion
 !real(r8), dimension(nlevdecomp), parameter   :: f_depth = 1!(/1.0, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4/)
-real(r8), parameter                          :: min_pool_value = 0.01
+real(r8), parameter                          :: min_pool_value = 1.0
 
 real(r8), dimension(pool_types), parameter   :: CN_ratio = (/15,15,5,8,20,20,20,11,8,11/) !Fungi/bacteria: Tang, Riley, Maggi 2019 as in Mouginot et al. 2014
                                                                                           !NOTE: Wallander/Rousk may have data more suited for Boreal/Arctic conditions
@@ -69,28 +70,34 @@ real(r8), dimension(pool_types), parameter   :: CN_ratio = (/15,15,5,8,20,20,20,
                                                                                           !SOM: From CLM documentation, table 21.3 (Mendeley version)
                                                                                           !LITm: MIMICS-CN manuscript
                                                                                           !LITs, ErM, AM: Guesses!
+
 !From Baskaran et al 2016
-real(r8), parameter :: yr_to_hr = 365*24        !Year to hour
-real(r8), parameter :: my_sap = 1/yr_to_hr      ![1/hr]mortality rate sap
-real(r8), parameter :: my_myc = 1/yr_to_hr      ![1/hr] mortality rate myc
-real(r8), parameter :: my_root = 0.15/yr_to_hr  ![1/hr]mortality rate plant root
-real(r8), parameter :: my_shoot = 0.15/yr_to_hr ![1/hr]  mortality rate plant shoot
+real(r8), parameter :: hr_pr_yr = 365*24        !For conversion
+real(r8), parameter :: my_sap = 1/hr_pr_yr      ![1/hr]mortality rate sap
+real(r8), parameter :: my_myc = 1/hr_pr_yr      ![1/hr] mortality rate myc
+real(r8), parameter :: my_root = 0.15/hr_pr_yr  ![1/hr]mortality rate plant root
+real(r8), parameter :: my_shoot = 0.15/hr_pr_yr ![1/hr]  mortality rate plant shoot
 real(r8), parameter :: gamma_rs = 0.3           !Plant root:shoot ratio                      TODO: Vary with plant type/pft/myc?
-real(r8)            :: delta                    !Fraction of plant C allocated to mycorrhiza TODO: determine based on myc type?
-real(r8), parameter :: a = 80                   !gC(gN)⁻¹yr⁻¹, max plant N productivity
-real(r8), parameter :: b = 0.09                 !Shading factor of plant productivity
+real(r8)            :: delta=0.25               !Fraction of plant C allocated to mycorrhiza TODO: determine based on myc type?
+real(r8), parameter :: a = 80/hr_pr_yr          !gC(gN)⁻¹yr⁻¹, max plant N productivity
+real(r8), parameter :: b = 0.01                 !Shading factor of plant productivity CHANGED from 0.09!
 real(r8), parameter :: Km_plant = 0.6           ![gNm-2] Half saturation constant of plant uptake of inorganic N (called S_p in article)
 real(r8), parameter :: Km_myc = 0.08            ![gNm-2] Half saturation constant of mycorrhizal uptake of inorganic N (called S_m in article)
-real(r8), parameter :: V_max_plant = 1.8        ![g g-1 yr-1] Max plant root uptake of inorganic N (called K_pn in article)
-real(r8), parameter :: V_max_myc = 1.8          ![g g-1 yr-1] Max mycorrhizal uptake of inorganic N (called K_mn in article)
-real(r8), parameter :: Leaching_rate = 3             ![gNm-2yr-1] Leaching rate
-real(r8), parameter :: Deposition_rate = 0.3         ![gNm-2yr-1] Deposition rate           NOTE: varied from 0.3-3 in article
+real(r8), parameter :: V_max_plant = 1.8/hr_pr_yr![g g-1 hr-1] Max plant root uptake of inorganic N (called K_pn in article)
+real(r8), parameter :: V_max_myc = 1.8/hr_pr_yr  ![g g-1 hr-1] Max mycorrhizal uptake of inorganic N (called K_mn in article)
+real(r8), parameter :: Leaching_rate = 3/hr_pr_yr             ![gNm-2hr-1] Leaching rate
+real(r8), parameter :: Deposition_rate = 1/hr_pr_yr         ![gNm-2hr-1] Deposition rate  NOTE: varied from 0.3-3 in article
 real(r8), parameter :: e_s = 0.25 !Growth efficiency of saprotrophs                         TODO: Compare these to the efficiencies from Mimics
 real(r8), parameter :: e_m = 0.25 !Growth efficiency of mycorrhiza
 !Decomposition rates:
-real(r8), parameter :: K_SH = 0.006 ![m2gC-1yr-1] Saprotrophic decay rate constant for hydrolizable store. TODO: review these
-real(r8), parameter :: K_MO = 0.003 ![m2gC-1yr-1] Mycorrhizal decay rate constant for oxidizable store     NOTE: vary from 0.0003 to 0.003 in article
+real(r8), parameter :: K_SH = 0.006/hr_pr_yr ![m2gC-1hr-1] Saprotrophic decay rate constant for hydrolizable store. TODO: review these
+real(r8), parameter :: K_MO = 0.003/hr_pr_yr ![m2gC-1hr-1] Mycorrhizal decay rate constant for oxidizable store     NOTE: vary from 0.0003 to 0.003 in article
 
+real(r8), parameter :: opt_Plant_CN = 50d0
+real(r8), parameter :: CPlant_min = 200d0
+real(r8), parameter :: NPlant_min = 4d0 !200/50 NOTE: Guesses, needs review
+
+!For reading soil temperature and moisture from CLM output file
 real(r8), dimension(nlevdecomp)          :: TSOIL
 real(r8), dimension(nlevdecomp)          :: SOILLIQ
 real(r8), dimension(nlevdecomp)          :: SOILICE
@@ -101,9 +108,10 @@ real(r8), dimension(nlevdecomp)          :: r_moist
 real(r8), parameter                          :: P = 44.247 !normalization of moisture function
 real(r8)                                     :: theta_l, theta_f, theta_sat ! liquid, frozen and saturation water content (Needs to come from some kind of forcing)
 real(r8)                                     :: gas_diffusion
-integer, parameter, dimension(12)           :: days_in_month =(/31,28,31,30,31,30,31,31,30,31,30,31/)
+integer, parameter, dimension(12)            :: days_in_month =(/31,28,31,30,31,30,31,31,30,31,30,31/)
 integer                                      :: current_month, previous_month
-!Fluxes between pools:
+
+!Fluxes etc:
 real(r8) :: C_LITmSAPb, C_LITsSAPb, C_EcMSOMp, C_EcMSOMa, C_EcMSOMc, C_ErMSOMp, C_ErMSOMa, C_ErMSOMc, C_AMSOMp, C_AMSOMa, C_AMSOMc, C_SOMaSAPb,C_SOMaSAPf, C_SOMpSOMa, C_SOMcSOMa
 real(r8) :: C_LITmSAPf, C_LITsSAPf
 real(r8) :: C_SAPbSOMa, C_SAPbSOMp, C_SAPbSOMc,C_SAPfSOMa, C_SAPfSOMp, C_SAPfSOMc
@@ -111,8 +119,8 @@ real(r8) :: N_LITmSAPb, N_LITsSAPb, N_EcMSOMp, N_EcMSOMa, N_EcMSOMc, N_ErMSOMp, 
 real(r8) :: N_LITmSAPf, N_LITsSAPf, N_SOMaEcM, N_SOMaErM,N_SOMaAM, N_PlantLITs, N_PlantLITm, N_INPlant, N_INEcM, N_INErM, N_INAM, N_EcMPlant, N_ErMPlant, N_AMPlant
 real(r8) :: N_SAPbSOMa, N_SAPbSOMp, N_SAPbSOMc,N_SAPfSOMa, N_SAPfSOMp, N_SAPfSOMc, N_SAPfIN, N_SAPbIN
 real(r8) :: C_growth_rate, C_PlantEcM, C_PlantErM, C_PlantAM, C_PlantLITm, C_PlantLITs, Decomp_ecm, Decomp_erm, Decomp_am, Leaching, Deposition
-real(r8) :: C_PR, C_PS, Total_plant_mortality,f, U_sb, U_sf
-!End fluxes between pools
+real(r8) :: C_PR, C_PS,N_PR, N_PS, Total_plant_mortality,f, U_sb, U_sf, CPlant, NPlant, P_N, Plant_CN, CPlant_tstep, NPlant_tstep
+
 character (len=*),parameter                  :: clm_data_file = '/home/ecaas/clm/cruncep_iso_hist/Dovre/clm50_clm50d001_1deg_CRUNCEPV7_iso_hist.clm2.h0.SOILLIQ_SOILICE_TSOI.Dovre2014.nc'
 !For writing to file:
 character (len=*),parameter                  :: output_path = '/home/ecaas/decomposition_results/'
@@ -123,5 +131,6 @@ character (len=*), dimension(pool_types),parameter:: an_variables = (/  "anLITm"
 character (len=*), dimension(29), parameter ::  name_fluxes = (/"LITmSAPb","LITmSAPf","LITsSAPb","LITsSAPf", "SAPbSOMp","SAPfSOMp", "SAPbSOMa","SAPfSOMa", "SAPbSOMc","SAPfSOMc", &
       "EcMSAPb ", "EcMSAPf ","ErMSAPb ","ErMSAPf ", "AMSAPb  ","AMSAPf  ","EcMSOMp ", "EcMSOMa ","EcMSOMc ", "ErMSOMp ","ErMSOMa ","ErMSOMc ","AMSOMp  ","AMSOMa  " &
       ,"AMSOMc  ","SOMaSAPb","SOMaSAPf","SOMpSOMa","SOMcSOMa" /)
+
 
 end module paramMod
