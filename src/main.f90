32f8b237c6d1cd2fda43c4451936ddcaf5d2c010
ecaas <ecaas@geo.uio.no>
2021-03-03 09:53:27 +0100

    src/main.f90

--- src/main.f90
+++ src/main.f90
@@ -4,8 +4,8 @@ program main
   implicit none
 
   integer :: N=100*365*24!Total number of timesteps (years,days, hours) *10 when step_frac 10
-  integer,parameter:: levels=1
+  integer,parameter:: levels=7
 
-  ! call decomp(nsteps=N, run_name="make_test2",nlevdecomp=levels, step_frac=1)
+  call decomp(nsteps=N, run_name="31486_multilayer",nlevdecomp=levels, step_frac=1)
 
 end program main
