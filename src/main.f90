bc08bdb0fbc2b77c01cc2c0d5733477e5f3831a3
ecaas <ecaas@geo.uio.no>
2021-08-24 10:01:56 +0200

    Output frequency moved to main call

--- src/main.f90
+++ src/main.f90
@@ -4,10 +4,9 @@ program main
   implicit none
 
 
-  integer :: N=1*365*24!Total number of timesteps (years,days, hours) *10 when step_frac 10
+  integer :: N=1*24*365!Total number of timesteps (years,days, hours) *10 when step_frac 10
   integer,parameter:: levels=10
-
-  call decomp(nsteps=N, run_name="test_after_merge",nlevdecomp=levels, step_frac=1)
+  call decomp(nsteps=N, run_name="test_mmk",nlevdecomp=levels, step_frac=1, write_hour=1*24*30)
 
 
 end program main
